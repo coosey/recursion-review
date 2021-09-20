caa19061bf701c3568d6236d408943a1c959a62d
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-20 16:35:21 -0700

    modifying If statement conditions for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,21 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+  
+  //if statement conditions {null, undefined, function, string number boolean}
+  if (obj === null) {
+    return 'null';
+  }
+  if (obj === undefined || typeof(obj) === 'function') {
+    return '';
+  }
+  if (typeof(obj) === 'boolean' || typeof(obj) === 'number' || typeof(obj) === 'string') {
+    return result.concat(obj);
+  }
+
+  //if (Array.isArray(obj))
+    // result += stringifJSON(obj[i])
+
+  //if (typeof obj === 'object')
 };
