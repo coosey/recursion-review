b87134200d218a6cc10b747baa96fae108000495
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-20 16:21:39 -0700

    implemented a for loop for recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,40 +4,8 @@
 // };
 // Must use classList & childNodes
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className, node = document) {
-  //var item = node;
+var getElementsByClassName = function(className, node = document.body) {
   var result = [];
-//var body = doc.body
-  //declare var hasChilds = body.hasChildNodes()
-  //if (hasChilds)
-
-// Interate over each node in body
-  // if(item.hasChildNodes()) {
-  //   for ( var i = 0; i < item.childNodes.length; i++) {
-  //     var currentNode = item.childNodes[i];
-      // if the node has a class list and that class list contains the class name
-      if (currentNode.classList && currentNode.classList.contains(className)) {
-        result.push(currentNode);
-      }
-      // if the node has child nodes
-      if (currentNode.childNodes) {
-        //for each child node, concat it to result array
-        var child = currentNode.childNodes;
-        child.forEach(function(child) {
-          if(!result.includes(child)) {
-            result = result.concat(getElementsByClassName(child, className));
-          }
-        });
-      }
-    };
-  } else {
-    if (node.classList && node.classList.contains(className)) {
-      result.push(node);
-    }
-    return result;
-    getElementsByClassName(document.body, className);
-  };
-  //-------------------------------------
   //if node.classList && node.classList.contains(className)
   if (node.classList && node.classList.contains(className)) {
     //push node into result
@@ -47,8 +15,8 @@ var getElementsByClassName = function(className, node = document) {
   if (node.childNodes) {
     //for loop thru node.children
     for (var i = 0; i < node.childNodes.length; i++) {
-      result = result.concat(getElementsByClassName(className, node.childNodes[i]))
+      result = result.concat(getElementsByClassName(className, node.childNodes[i]));
     }
   }
-  //return node
+  return result;
 }
