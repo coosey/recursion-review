544deacf14244756f42c4d0b33f9f1272f2a3cf8
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-20 11:22:10 -0700

    Trying to implement our function to loop through the body nodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,20 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
-
+// Must use classList & childNodes
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var body = document.body;
+  var result = [];
+// Interate over each node in body
+for ( var i = 0; i < body.childNodes.length; i++) {
+  var currentNode = body.childNodes[i];
+  // if the node has a class list and that class list contains the class name
+  if (currentNode.classList && currentNode.classList.contains(className)) {
+    result.push(currentNode);
+  }
+}
+// if the node has child nodes
+  //for each child node, concat it to result array
+  result = result.concat(getElementsByClassName(currentNode));
 };
